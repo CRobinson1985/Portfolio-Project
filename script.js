bfd531e65762127dbc7f272fa24ca61f6d1bce2e
crobinson1985 <crobinson92828@gmail.com>
2025-06-10 20:20:50 -0400

    v0.19 Hid the pointing hands for the more professional look, still there for the silly side.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const bust = document.getElementById("bust");
+const hands = document.querySelectorAll(".hand")
 let bustChecker = 'Myself';
 const h1 = document.querySelector('h1');
 const h2 = document.querySelector('h2');
 const loadingBackdrop = document.getElementById('loader-backdrop');
+
 
 window.addEventListener('DOMContentLoaded', function() {
   this.setTimeout(function() {
@@ -20,11 +22,16 @@ function bustClickHandler() {
     bustChecker = 'cool-guy';
     h1.innerText = "Welcome To This Cool Guy's Portfolio";
     h2.innerText = "Innovative. Bold. Slightly Ridiculous.";
+    hands[0].classList.remove('hidden');
+    hands[1].classList.remove('hidden');
+
     
   } else {
     bust.src = './assets/pics/Myself.jpg';
     bustChecker = 'Myself';
     h1.innerText = "Welcome to My Professional Portfolio";
     h2.innerText = "Building the future through creativity, technology, and a little fun!";
+    hands[0].classList.add('hidden');
+    hands[1].classList.add('hidden');
   }
 }
